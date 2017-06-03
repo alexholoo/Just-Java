78652af2284ea5fb597e1f52520e903cdd32c32f
He Huang <he.huang.ucandoit@gmail.com>
2017-06-03 09:46:51 -0400

    add price TextView

--- app/src/main/java/com/example/h94hu/justjava/MainActivity.java
+++ app/src/main/java/com/example/h94hu/justjava/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        display(2);
     }
 
     /**
@@ -29,5 +29,11 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+        display_price(number);
+    }
+
+    private void display_price(int number){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText("$"+ number * 5);
     }
 }
