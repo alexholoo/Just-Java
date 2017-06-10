e61a2183a4fc82a084ec400f0c8e76dad8d38f35
He Huang <he.huang.ucandoit@gmail.com>
2017-06-10 14:22:29 -0400

    Added 2 buttons to change quantity and price accordingly.

--- app/src/main/java/com/example/h94hu/justjava/MainActivity.java
+++ app/src/main/java/com/example/h94hu/justjava/MainActivity.java
@@ -4,36 +4,63 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvQuantity;
+    TextView tvPrice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvQuantity = (TextView) findViewById(R.id.tvQuantity);
+        tvPrice = (TextView) findViewById(R.id.tvPrice);
+        tvQuantity.setText(0 + "");
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-    }
-
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-        display_price(number);
+        toastMessage("Your order is submitted!");
     }
 
     private void display_price(int number){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText("$"+ number * 5);
+        tvPrice.setText("$"+ number * 5);
+    }
+
+    public void increaseQuantity(View view) {
+        int currentQuantity = Integer.parseInt(tvQuantity.getText().toString());
+        int newQuantity = currentQuantity + 1;
+        tvQuantity.setText( newQuantity + "");
+
+        display_price(newQuantity);
+    }
+
+    /*
+    * Toast a message
+    * */
+    public void toastMessage (String msg) {
+        Toast.makeText(this,msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void decreaseQuantity(View view) {
+        int currentQuantity = Integer.parseInt(tvQuantity.getText().toString());
+        int newQuantity;
+        if(currentQuantity == 0){
+            newQuantity = 0;
+            toastMessage("Don't you like to order one?");
+        }else{
+            newQuantity = currentQuantity - 1;
+        }
+        tvQuantity.setText( newQuantity + "");
+
+        display_price(newQuantity);
     }
 }
